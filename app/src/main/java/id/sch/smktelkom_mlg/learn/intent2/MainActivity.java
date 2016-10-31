60bb3874f96a1f4b3a2e6a85934b274f6e3dbd43
Dita Ayu <ditaayu0609@gmail.com>
2016-10-31 22:45:25 +0700

    startActivityForResult Final

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent2/MainActivity.java
@@ -1,13 +1,40 @@
 package id.sch.smktelkom_mlg.learn.intent2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQUEST_CODE)
+        {
+            if (resultCode==RESULT_OK)
+            {
+                String nama = data.getStringExtra(InputActivity.NAMA);
+                int thnLhr = data.getIntExtra(InputActivity.TAHUN_LAHIR);
+                TextView tvBiodata = (TextView) findViewById(R.id.textViewBiodata);
+                tvBiodata.setText("Nama\t\t\t\t\t\t\t\t: "+nama
+                        +"\nTahun Kelahiran\t: "+thnLhr);
+            }
+        }
     }
 }
